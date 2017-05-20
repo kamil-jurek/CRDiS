bce3158e3aa970efabfbce69b878f1cb2b8b0156
Kamil <keyjay624@gmail.com>
2017-05-20 13:51:42 +0200

    plotSequence function added

--- GeneratorTest.py
+++ GeneratorTest.py
@@ -1,20 +1,31 @@
 import SequenceGenerator
 import matplotlib.pyplot as plt
 import numpy
+import math
 
-args = [1, 2, 3, 4, 5, 6]
-args_op = ['eq','eq','eq','eq','eq','eq']
-args_prob = [0.1, 0.8, 1.0, 0.6, 0.5, 0.93]
-args_len = [100, 50, 30, 20, 10, 50]
+domain1 = ['a', 'b', 'g']
+domain2 = ['c', 'd']
+domain3 = ['e', 'f']
 
-seq = [SequenceGenerator.generateSequence(args,
-                                          args[i],
-                                          args_op[i],
-                                          args_prob[i],
-                                          args_len[i]) for i in range(len(args))]
+states_len = [500, 30]
+curr_state = max(states_len)*2
+last_state = curr_state + max(states_len)
 
-print seq
-[plt.plot(seq[i],'ro', color=numpy.random.rand(3,1)) for i in range(len(args))]
+
+seq01 = ['a' for i in range(0, last_state)]
+seq1 = SequenceGenerator.generateSequence(seq01, domain1,'b','eq',0.9, curr_state-states_len[0], last_state)
+
+seq02 = ['c' for i in range(0, last_state)]
+seq2 = SequenceGenerator.generateSequence(seq02, domain2,'d','eq',0.8, curr_state-states_len[1], last_state)
+
+seq03 = ['e' for i in range(0, last_state)]
+seq3 = SequenceGenerator.generateSequence(seq03, domain3,'f','eq',1.0, curr_state, last_state)
+
+f, axarr = plt.subplots(3)
+
+SequenceGenerator.plotSequence(axarr[0], seq1, domain1, '1', curr_state)
+SequenceGenerator.plotSequence(axarr[1], seq2, domain2, '2', curr_state)
+SequenceGenerator.plotSequence(axarr[2], seq3, domain3, '3', curr_state)
+
+#f.set_size_inches(w=10,h=5)
 plt.show()
-
-[SequenceGenerator.testSequence(seq[i], args[i], args_prob[i]) for i in range(len(args))]
--- SequenceGenerator.py
+++ SequenceGenerator.py
@@ -1,16 +1,38 @@
 import numpy
+import math
 
-def generateSequence(domain, value, op, probability, numberOfSamples, next=False):
-    if op == 'eq':
+def generateSequence(sequence, domain, value, operator, probability, fr, to):
+    if operator == 'eq':
         prob = [(1.0 - probability) / (len(domain) - 1) for i in domain]
         prob[domain.index(value)] = probability
 
-        seq = [numpy.random.choice(domain, p = prob) for j in range(0, numberOfSamples)]
+        for i in range(fr, to):
+            sequence[i] = numpy.random.choice(domain, p = prob)
 
-        return seq
+        return sequence
 
 def testSequence(seq, value, prob):
     counter = seq.count(value)
 
     print "Should be about: ", len(seq)*prob
     print "Is: ", counter
+
+def plotSequence(axarr, seq, domain, attrName, curr_state):
+    N = 1
+    colors = [numpy.random.rand(3,1) for i in domain]
+    ind = numpy.arange(N)    # the x locations for the groups
+
+    p = [0 for i in domain]
+    for i in range(0, len(seq)):
+        for j in range(0, len(domain)):
+            if seq[i] == domain[j]:
+                p[j] = axarr.barh(ind, 1, left=i, color=colors[j] )
+
+    axarr.set_xlabel('Liczba probek')
+    axarr.set_ylabel('Attribute '+attrName)
+    axarr.set_yticks([])
+    axarr.set_yticklabels(['Attr_1'])
+
+    #axarr.set_xticklabels(numpy.arange(-curr_state, len(seq)-curr_state+1, 10),minor=False)
+    axarr.set_xticks(numpy.arange(0, len(seq)+1, math.floor(len(seq)/10)),minor=False)
+    axarr.legend((p), (domain))
