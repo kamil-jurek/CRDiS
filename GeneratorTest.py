67c79417c932aedaddc55502932f96ff22c67eee
Kamil <keyjay624@gmail.com>
2017-05-20 20:14:10 +0200

    saving to csv added

--- GeneratorTest.py
+++ GeneratorTest.py
@@ -29,3 +29,12 @@ sg.plotSequence(axarr[2], seq3, domain3, '3', curr_state)
 
 #f.set_size_inches(w=10,h=5)
 plt.show()
+
+import csv
+import time
+
+timestr = time.strftime("%Y_%m_%d-%H:%M:%S")
+with open('sequence_'+timestr+'.csv','wb') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    for i in range(len(seq1)):
+        writer.writerow([i,seq1[i],seq2[i],seq3[i]])
